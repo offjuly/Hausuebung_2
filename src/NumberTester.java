8c18dbacde747a693c68fb8460118eb6ad923f6f
offjuly <baumgartnerj19000@sus.htl-grieskirchen.at>
2021-09-27 12:00:07 +0200

    4th commit

--- src/NumberTester.java
+++ src/NumberTester.java
@@ -1,8 +1,24 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class NumberTester implements NumberTest{
 
 
-    public NumberTester(String fileName){
-        //TODO
+    private NumberTest palindromeTester;
+    private NumberTester oddTester;
+    private NumberTester primeTester;
+
+    public NumberTester(String myFile){
+        try( BufferedReader br = new BufferedReader(new FileReader("myFile.txt"));) {
+           String line = br.readLine();
+           
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void setOddEvenTester(NumberTester oddTester){
