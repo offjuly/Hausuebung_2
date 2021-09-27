01c234722449c54dafe2611cbc491b005fb23d1f
offjuly <baumgartnerj19000@sus.htl-grieskirchen.at>
2021-09-27 11:06:43 +0200

    sec commit

--- src/HalloJavamitForEach.java
+++ src/HalloJavamitForEach.java
@@ -2,7 +2,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class HalloJavamitForEach {
-    List<String> myList = new ArrayList<>();
+    private List<String> myList = new ArrayList<>();
 
-    
+    public HalloJavamitForEach() {
+        myList.add("a");
+        myList.add("b");
+        myList.add("c");
+        myList.add("d");
+    }
+
+    public void showList() {
+        for (String s : myList
+        ) {
+            System.out.println(s);
+        }
+
+        myList.forEach((String s) -> System.out.println(s));
+
+        myList.forEach(System.out::print);
+
+    }
+
 }
